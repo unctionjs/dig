3a2f6f78e6b95c7672452e0fec9ee61e12f2f5fe
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 23:46:21 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import get from "@unction/get"
-import reduceValues from "@unction/reducevalues"
-import flip from "@unction/flip"
-
-export default function dig (keychain: KeyChainType): Function {
-  return function digKeyChain (tree: TreeType): ValueType {
-    return reduceValues(flip(get))(tree)(keychain)
-  }
+import get from "@unction/get";
+import reduceValues from "@unction/reducevalues";
+import flip from "@unction/flip";
+export default function dig(keychain) {
+  return function digKeyChain(tree) {
+    return reduceValues(flip(get))(tree)(keychain);
+  };
 }
